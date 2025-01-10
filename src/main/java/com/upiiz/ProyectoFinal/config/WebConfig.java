60f5a66f17f7b9fd2cef35b93ceea17ef7611226
Miguel Hernandez <jesmiguelhdzg@gmail.com>
2025-01-10 00:25:34 -0600

    v1.2

---
+++ src/main/java/com/upiiz/ProyectoFinal/config/WebConfig.java
@@ -0,0 +1,22 @@
+package com.upiiz.ProyectoFinal.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(org.springframework.web.servlet.config.annotation.CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("*")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
