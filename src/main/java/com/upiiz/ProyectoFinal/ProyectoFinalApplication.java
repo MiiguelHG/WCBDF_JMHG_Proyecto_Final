9a073eb5b84b2cc1961a320680d492cbbed6af7a
Miguel Hernandez <jesmiguelhdzg@gmail.com>
2025-01-10 11:34:59 -0600

    v1.5

--- src/main/java/com/upiiz/ProyectoFinal/ProyectoFinalApplication.java
+++ src/main/java/com/upiiz/ProyectoFinal/ProyectoFinalApplication.java
@@ -1,6 +1,8 @@
 package com.upiiz.ProyectoFinal;
 
+import com.upiiz.ProyectoFinal.entities.InvoiceEntity;
 import com.upiiz.ProyectoFinal.entities.UserEntity;
+import com.upiiz.ProyectoFinal.repositories.InvoiceRepository;
 import com.upiiz.ProyectoFinal.repositories.UserRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -8,6 +10,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.core.userdetails.User;
 
+import java.sql.Date;
 import java.util.List;
 
 @SpringBootApplication
@@ -18,7 +21,7 @@ public class ProyectoFinalApplication {
 	}
 
 	@Bean
-	CommandLineRunner init(UserRepository userRepository) {
+	CommandLineRunner init(UserRepository userRepository, InvoiceRepository invoiceRepository) {
 		return args -> {
 			UserEntity admin = UserEntity
 					.builder()
@@ -57,6 +60,50 @@ public class ProyectoFinalApplication {
 					.build();
 
 			userRepository.saveAll(List.of(admin, user, moderator, editor, developer, analyst));
+
+			InvoiceEntity invoice1 = InvoiceEntity
+					.builder()
+					.invoiceDate(Date.valueOf("2021-10-01"))
+					.amount(1050.25)
+					.CustomerId(68L)
+					.build();
+
+			InvoiceEntity invoice2 = InvoiceEntity
+					.builder()
+					.invoiceDate(Date.valueOf("2024-05-02"))
+					.amount(2520.99)
+					.CustomerId(8L)
+					.build();
+
+			InvoiceEntity invoice3 = InvoiceEntity
+					.builder()
+					.invoiceDate(Date.valueOf("2024-11-11"))
+					.amount(750.25)
+					.CustomerId(30L)
+					.build();
+
+			InvoiceEntity invoice4 = InvoiceEntity
+					.builder()
+					.invoiceDate(Date.valueOf("2021-10-04"))
+					.amount(2371.00)
+					.CustomerId(123L)
+					.build();
+
+			InvoiceEntity invoice5 = InvoiceEntity
+					.builder()
+					.invoiceDate(Date.valueOf("2025-01-05"))
+					.amount(590.75)
+					.CustomerId(12L)
+					.build();
+
+			InvoiceEntity invoice6 = InvoiceEntity
+					.builder()
+					.invoiceDate(Date.valueOf("2024-12-28"))
+					.amount(900.25)
+					.CustomerId(44L)
+					.build();
+
+			invoiceRepository.saveAll(List.of(invoice1, invoice2, invoice3, invoice4, invoice5, invoice6));
 		};
 	}
 
--- src/main/java/com/upiiz/ProyectoFinal/entities/UserEntity.java
+++ src/main/java/com/upiiz/ProyectoFinal/entities/UserEntity.java
@@ -16,6 +16,8 @@ public class UserEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(unique = true)
     private String username;
     private String password;
 }
